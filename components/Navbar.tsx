0191937aa141cadb651b4de8a33569c13a65ccd9
Leon <junzai12@icloud.com>
2023-04-12 20:29:46 -0700

    small navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className="">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+
+export default Header;
