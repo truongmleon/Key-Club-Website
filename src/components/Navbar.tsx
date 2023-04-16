dc99c28eef4d2a0a195449959886a3dccbfd6166
Leon <junzai12@icloud.com>
2023-04-15 17:46:28 -0700

    nav problems

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
+let navRows: string[] = ["Home", "About KC", "Meet the Board", "Resources", "Announcements", "Contact"];
+let htmlNav: React.ReactElement<any, string | React.JSXElementConstructor<any>> | JSX.Element[] = [];
+
+for (let i = 0; i < navRows.length; i++) {
+  htmlNav.push(<li><a href="">{navRows[i]}</a></li>);
+}
+
 const Navbar = () => {
   return (
     <div className="">
       <nav>
-        <ul>
-          <li>Home</li>
-          <li>About KC</li>
-          <li>Meet the Board</li>
-          <li>Resources</li>
-          <li>Announcements</li>
-          <li>Contact</li>
-        </ul>
+        {htmlNav}
       </nav>
     </div>
   )
