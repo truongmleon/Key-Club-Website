43958e80cfbbbccb21028e521749eade3e66c7c7
Leon <junzai12@icloud.com>
2023-04-15 17:58:57 -0700

    skelly navbar for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ let navRows: string[] = ["Home", "About KC", "Meet the Board", "Resources", "Ann
 let htmlNav: React.ReactElement<any, string | React.JSXElementConstructor<any>> | JSX.Element[] = [];
 
 for (let i = 0; i < navRows.length; i++) {
-  htmlNav.push(<li><a href="">{navRows[i]}</a></li>);
+  htmlNav.push(<li><a href={"#" + navRows[i].toLowerCase()}>{navRows[i]}</a></li>);
 }
 
 const Navbar = () => {
