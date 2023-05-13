257f53dd87df53792454ea847386d55425a25724
Leon <junzai12@icloud.com>
2023-05-13 16:11:20 -0700

    what am ia doing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+let load = (id: string): React.MouseEventHandler<HTMLAnchorElement> => {
+  throw new Error("Function not implemented.");
+}
+
 let navRows: string[] = ["Home", "Meet the Board", "Resources", "Announcements", "Contact"];
 let htmlNav: React.ReactElement<any, string | React.JSXElementConstructor<any>> | JSX.Element[] = [];
 
 for (let i = 0; i < navRows.length; i++) {
-  htmlNav.push(<li><a href={"#" + navRows[i].toLowerCase()}>{navRows[i]}</a></li>);
+  htmlNav.push(<li><a onClick={load(navRows[i].toLowerCase())} href={"#" + navRows[i].toLowerCase()}>{navRows[i]}</a></li>);
 }
 
 const Navbar = () => {
   return (
     <div className="">
+    
       <nav>
         <img id="logo" src="src/images/logo.png" alt="Key Club Logo" />
         <ul>
@@ -21,3 +26,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
---
+++ src/components/home.tsx
@@ -0,0 +1,7 @@
+const home = () => {
+    <div>
+        <img id="logo" src="src/images/kwkc.png" alt="Key Club Letters" />
+    </div>
+}
+
+export default home;
