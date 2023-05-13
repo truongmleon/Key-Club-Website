7b830515ca763a5c2510e5867a123ec5930c639c
Leon <junzai12@icloud.com>
2023-05-13 13:36:43 -0700

    navbar looking more hefty

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-let navRows: string[] = ["Home", "About KC", "Meet the Board", "Resources", "Announcements", "Contact"];
+let navRows: string[] = ["Home", "About", "Meet the Board", "Resources", "Announcements", "Contact"];
 let htmlNav: React.ReactElement<any, string | React.JSXElementConstructor<any>> | JSX.Element[] = [];
 
 for (let i = 0; i < navRows.length; i++) {
