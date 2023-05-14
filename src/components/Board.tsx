4d8118a5d09a09d9494fb8775094ecb9206f4fb1
Leon <junzai12@icloud.com>
2023-05-13 23:30:47 -0700

    styles organization

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,7 @@
 const Board = () => {
     return (
         <div>
-        <img src="src/images/kwkc.png" alt="Key Club Letters" />
+        
         </div>
     )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div id="home">
+            <img id="kctext" src="src/images/kwkc.png" alt="Key Club Letters" />
+            <h1 id="title">Kentwood High School</h1>    
+        </div>
+    )
+}
+
+export default Home;
