b4b4b2cd2839c46782b102835e9c69fd58a6147a
Leon <junzai12@icloud.com>
2023-05-15 10:43:41 -0700

    resources pages

---
+++ src/components/Resources.tsx
@@ -0,0 +1,13 @@
+const Resources = () => {
+    return (
+        <div id='resources-page'>
+            <h1 id="resources-title">Resources</h1>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
+
+export default Resources;
