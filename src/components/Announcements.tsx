c3d5d869e76124b4f676eed3b26a6878dbee8893
Leon <junzai12@icloud.com>
2023-06-24 16:43:16 -0700

    yo got most of the annoucements done

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,8 +1,23 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function App() {
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+}
+
 const Announcements = () => {
     return (
-        <div>
-            
-            
+        <div id="announcements-page">
+            <h1 id="announcements-title">View the Instagram!</h1>
+            <h2 id="disclaimer">May not work under KSD internet :(</h2>
+
+            <div className="flex">
+            <h1 data-aos="zoom-in" id="remind-title">Join the Remind!</h1>
+            </div>
         </div>
     )
 }
