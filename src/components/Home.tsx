16e857f44119d3fb879b7efcd964a7e6baed7042
Leon <junzai12@icloud.com>
2023-06-24 00:33:07 -0700

    animations finally work

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <div id="home">
             <br/>
-            <h1 data-aos="zoom-in-up" id="title">Kentwood High School</h1>   
+            <h1 id="title">Kentwood High School</h1>   
         </div>
     )
 }
