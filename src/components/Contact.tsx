18b0f997b4074c6842c28810ca23a81dfeb84abf
Leon <junzai12@icloud.com>
2023-06-25 15:57:38 -0700

    added board members

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,15 +4,15 @@ const Contact = () => {
             <br/>
             <h1 id="contact-title">Looking for someone?</h1>  
 
-            <ul>
+            <ul id="board-list">
                 <li id="board-title">Key Club Board</li>
-                <li>Anthony Luong (President): email || <a href="https://www.instagram.com/antthienluong">Instagram</a></li>
-                <li>Jason Mai (Vice President): jasonMai52007@gmail.com || <a href="https://www.instagram.com/jason.mai07">Instagram</a></li>
-                <li>Vicky Li (Secretary): vickyli354949@gmail.com || <a href="https://www.instagram.com/vickkyyy_">Instagram</a></li>
-                <li>Anissa Vichitthavong (Treasurer): anissavichitthavong@gmail.com || <a href="https://www.instagram.com/anissavichitthavong">Instagram</a></li>
-                <li>Emily Xu (Bulletin Editor): emilyxu567@gmail.com || <a href="https://www.instagram.com/em.xux">Instagram</a></li>
-                <li>Leon Truong (Webmaster): leonkeyclub1@gmail.com || <a href="https://www.instagram.com/leonmmtt/">Instagram</a></li>
-                <li>Meena Ha (Fundraising Head): meenaha0223@gmail.com || <a href="https://www.instagram.com/mha.vp">Instagram</a></li>
+                <li>Anthony Luong (President) --{">"} email || <a href="https://www.instagram.com/antthienluong" target="_blank">Instagram</a></li>
+                <li>Jason Mai (Vice President) --{">"} <i>jasonmai52007@gmail.com </i>|| <a href="https://www.instagram.com/jason.mai07" target="_blank">Instagram</a></li>
+                <li>Vicky Li (Secretary) --{">"} <i>vickyli354949@gmail.com</i> || <a href="https://www.instagram.com/vickkyyy_" target="_blank">Instagram</a></li>
+                <li>Anissa Vichitthavong (Treasurer) --{">"} <i>anissavichitthavong@gmail.com</i> || <a href="https://www.instagram.com/anissavichitthavong" target="_blank">Instagram</a></li>
+                <li>Emily Xu (Bulletin Editor) --{">"} <i>emilyxu567@gmail.com</i> || <a href="https://www.instagram.com/em.xux" target="_blank">Instagram</a></li>
+                <li>Leon Truong (Webmaster) --{">"} <i>leonkeyclub1@gmail.com</i> || <a href="https://www.instagram.com/leonmmtt/" target="_blank">Instagram</a></li>
+                <li>Meena Ha (Fundraising Head) --{">"} <i>meenaha0223@gmail.com</i> || <a href="https://www.instagram.com/mha.vp" target="_blank">Instagram</a></li>
             </ul> 
         </div>
     )
