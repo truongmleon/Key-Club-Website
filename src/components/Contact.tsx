99391118499816ad71b06653a787f3f6c91b625a
Leon <junzai12@icloud.com>
2023-06-25 15:46:52 -0700

    board list

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,18 @@ const Contact = () => {
     return (
         <div id="contact">
             <br/>
-            <h1 id="contact-title">Looking for someone?</h1>   
+            <h1 id="contact-title">Looking for someone?</h1>  
+
+            <ul>
+                <li id="board-title">Key Club Board</li>
+                <li>Anthony Luong (President): email || <a href="https://www.instagram.com/antthienluong">Instagram</a></li>
+                <li>Jason Mai (Vice President): jasonMai52007@gmail.com || <a href="https://www.instagram.com/jason.mai07">Instagram</a></li>
+                <li>Vicky Li (Secretary): vickyli354949@gmail.com || <a href="https://www.instagram.com/vickkyyy_">Instagram</a></li>
+                <li>Anissa Vichitthavong (Treasurer): anissavichitthavong@gmail.com || <a href="https://www.instagram.com/anissavichitthavong">Instagram</a></li>
+                <li>Emily Xu (Bulletin Editor): emilyxu567@gmail.com || <a href="https://www.instagram.com/em.xux">Instagram</a></li>
+                <li>Leon Truong (Webmaster): leonkeyclub1@gmail.com || <a href="https://www.instagram.com/leonmmtt/">Instagram</a></li>
+                <li>Meena Ha (Fundraising Head): meenaha0223@gmail.com || <a href="https://www.instagram.com/mha.vp">Instagram</a></li>
+            </ul> 
         </div>
     )
 }
