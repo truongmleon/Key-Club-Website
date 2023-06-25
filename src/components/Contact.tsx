d2b6dd57ff00662692f1310052e5c7cfe3bd61c1
Leon <junzai12@icloud.com>
2023-06-25 13:30:35 -0700

    contact setup

---
+++ src/components/Contact.tsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div id="contact">
+            <br/>
+            <h1>Kentwood High School</h1>   
+        </div>
+    )
+}
+
+export default Contact;
