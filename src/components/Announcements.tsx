1be4af55d4f8cc88e97e35278b469a1c1a0588ed
Leon <junzai12@icloud.com>
2023-06-24 18:31:50 -0700

    remind updates

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -2,21 +2,18 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-function App() {
+const Announcements = () => {
     useEffect(() => {
         AOS.init();
-        AOS.refresh();
-    }, []);
-}
+      }, [])
 
-const Announcements = () => {
     return (
         <div id="announcements-page">
             <h1 id="announcements-title">View the Instagram!</h1>
             <h2 id="disclaimer">May not work under KSD internet :(</h2>
 
-            <div className="flex">
-            <h1 data-aos="zoom-in" id="remind-title">Join the Remind!</h1>
+            <div data-aos="fade-down" className="flex">
+            <h1 id="remind-title">Join the Remind!</h1>
             </div>
         </div>
     )
