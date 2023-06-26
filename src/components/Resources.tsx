29a936bed3f8dcb6eac180cada5c8e91cc398efd
Leon <junzai12@icloud.com>
2023-06-26 15:07:51 -0700

    Resources title

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,11 +1,12 @@
 const Resources = () => {
     return (
         <div id='resources-page'>
-            <h1 id="resources-title">Resources</h1>
-            <button id="service-form"></button>
-            <button></button>
-            <button></button>
-            <button></button>
+            <h1 id="resources-title">Looking for something?</h1>
+            <h2 id="note">Some quick things to note..</h2>
+            <ul>
+                <li>Meetings held in Mr. White's room (565) before & after-school on Mondays (see below for dates)</li>
+                <li>Members require 13-15 hours per year to stay as a member of Key Club</li>
+            </ul>
         </div>
     )
 }
