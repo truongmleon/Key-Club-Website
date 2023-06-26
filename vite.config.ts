77488043b027e4627261e1f827bd9d4509e7505b
Leon <junzai12@icloud.com>
2023-06-25 18:03:15 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        resources: 'resources.html',
+        announcements: 'announcements.html',
+        contact: 'contact.html',
+      },
+    },
+  },
 })
