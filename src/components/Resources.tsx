d4f5d3baf3424443e7e77614de325b0fa2361b8e
Leon <junzai12@icloud.com>
2023-06-26 15:18:43 -0700

    added quick notes

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -3,9 +3,10 @@ const Resources = () => {
         <div id='resources-page'>
             <h1 id="resources-title">Looking for something?</h1>
             <h2 id="note">Some quick things to note..</h2>
-            <ul>
+            <ul id="note-list">
                 <li>Meetings held in Mr. White's room (565) before & after-school on Mondays (see below for dates)</li>
                 <li>Members require 13-15 hours per year to stay as a member of Key Club</li>
+                <li>A meeting consists of slides with service opportunities, quick announcements, activities (in afternoon meetings), contact information, and more!</li>
             </ul>
         </div>
     )
